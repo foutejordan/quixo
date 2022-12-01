1bc884f5ad0d8e44a87aefdb66914c22c886bd17
ftjordan <foutetapejordan10@gmail.com>
2022-12-02 00:12:37 +0100

    init

---
+++ src/main/java/com/quixo/projet_quixo/HelloController.java
@@ -0,0 +1,77 @@
+package com.quixo.projet_quixo;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+    Random random = new Random();
+
+    //AdversarialSearchTicTacToe ticTacToeAI = new AdversarialSearchTicTacToe();
+
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Button button4;
+    @FXML
+    private Button button5;
+    @FXML
+    private Button button6;
+    @FXML
+    private Button button7;
+    @FXML
+    private Button button8;
+    @FXML
+    private Button button9;
+    @FXML
+    private Button button10;
+    @FXML
+    private Button button11;
+    @FXML
+    private Button button12;
+    @FXML
+    private Button button13;
+    @FXML
+    private Button button14;
+    @FXML
+    private Button button15;
+    @FXML
+    private Button button16;
+    @FXML
+    private Button button17;
+    @FXML
+    private Button button18;
+    @FXML
+    private Button button19;
+    @FXML
+    private Button button20;
+    @FXML
+    private Button button21;
+    @FXML
+    private Button button22;
+    @FXML
+    private Button button23;
+    @FXML
+    private Button button24;
+    @FXML
+    private Button button25;
+    @FXML
+    private Text winnerText;
+
+    ArrayList<Button> pions;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
